04a465acea18518817b0ccc770dc1cdb43e184c2
Bas Roding <bas.roding@gmail.com>
2018-10-11 15:59:12 +0200

    Added some proper tests

--- Assets/Tests/ContainerTests.cs
+++ Assets/Tests/ContainerTests.cs
@@ -2,19 +2,131 @@
 using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class ContainerTests {
 
-    [Test]
-    public void ContainerTestsSimplePasses() {
+    [TearDown]
+    public void DestroyExistingContainer() {
+        TestContainer testContainer = GameObject.FindObjectOfType<TestContainer>();
+        if(testContainer != null) {
+            GameObject.DestroyImmediate(testContainer.gameObject);
+        }
     }
 
-    // A UnityTest behaves like a coroutine in PlayMode
-    // and allows you to yield null to skip a frame in EditMode
+    private TestContainer CreateTestContainer() {
+        GameObject containerGo = new GameObject();
+        GameObject itemGo = new GameObject();
+        itemGo.transform.SetParent(containerGo.transform);
+        TestContainer container = containerGo.AddComponent<TestContainer>();
+        TestItem item = itemGo.AddComponent<TestItem>();
+        container.ContainerItemTemplate = item;
+        container.Initialize();
+
+        return container;
+    }
+
+    private List<Weapon> CreateWeaponsList(int amount) {
+        List<Weapon> weapons = new List<Weapon>();
+
+        for(int i = 0; i < amount; i++) {
+            weapons.Add(new Weapon());
+        }
+
+        return weapons;
+    }
+
+    [Test]
+    public void UpdateContainer_IsItemCountCorrect() {
+        TestContainer container = CreateTestContainer();
+        IEnumerable<Weapon> weapons = CreateWeaponsList(5);
+        container.UpdateContainer(weapons);
+
+        Assert.AreEqual(container.Items.Count(), 5);
+    }
+
+    [Test]
+    public void UpdateContainer_AreGameObjectsCorrectlySpawned() {
+        TestContainer container = CreateTestContainer();
+        IEnumerable<Weapon> weapons = CreateWeaponsList(5);
+        container.UpdateContainer(weapons);
+
+        Assert.AreEqual(5, GameObject.FindObjectsOfType<TestItem>().Length);
+    }
+
+    [Test]
+    public void DestroyItem_IsItemDestroyed() {
+        TestContainer container = CreateTestContainer();
+        IEnumerable<Weapon> weapons = CreateWeaponsList(5);
+        container.UpdateContainer(weapons);
+        container.DestroyItem(weapons.First());
+
+        Assert.AreEqual(container.Items.Count(), 4);
+    }
+
+    [Test]
+    public void DestroyItem_IsItemsEmpty() {
+        TestContainer container = CreateTestContainer();
+        List<Weapon> weapons = CreateWeaponsList(5);
+        container.UpdateContainer(weapons);
+
+        for(int i = 0; i < 5; i++) {
+            Weapon weaponToDelete = weapons.First();
+            container.DestroyItem(weaponToDelete);
+            weapons.Remove(weaponToDelete);
+        }
+
+        Assert.AreEqual(0, container.Items.Count());
+    }
+
     [UnityTest]
-    public IEnumerator ContainerTestsWithEnumeratorPasses() {
-        // Use the Assert class to test conditions.
-        // yield to skip a frame
-        yield return null;
+    public IEnumerator DestroyItem_AreItemGameObjectsDestroyed() {
+        TestContainer container = CreateTestContainer();
+        List<Weapon> weapons = CreateWeaponsList(5);
+        container.UpdateContainer(weapons);
+
+        for (int i = 0; i < 5; i++) {
+            Weapon weaponToDelete = weapons.First();
+            container.DestroyItem(weaponToDelete);
+            weapons.Remove(weaponToDelete);
+        }
+
+        yield return new WaitForEndOfFrame();
+
+        Assert.AreEqual(0, GameObject.FindObjectsOfType<TestItem>().Length);
+    }
+
+    [Test]
+    public void Clear_IsItemsEmpty() {
+        TestContainer container = CreateTestContainer();
+        IEnumerable<Weapon> weapons = CreateWeaponsList(5);
+        container.UpdateContainer(weapons);
+        container.Clear();
+
+        Assert.AreEqual(0, container.Items.Count());
+    }
+
+    [Test]
+    public void CreateItem_IsItemsCountIncreased() {
+        TestContainer container = CreateTestContainer();
+        IEnumerable<Weapon> weapons = CreateWeaponsList(5);
+        container.UpdateContainer(weapons);
+        Weapon weapon = new Weapon();
+        container.CreateItem(weapon);
+
+        Assert.AreEqual(6, container.Items.Count());
+    }
+
+    [UnityTest]
+    public IEnumerator Clear_AreGameObjectsDestroyed() {
+        TestContainer container = CreateTestContainer();
+        IEnumerable<Weapon> weapons = CreateWeaponsList(5);
+        container.UpdateContainer(weapons);
+        container.Clear();
+
+        yield return new WaitForEndOfFrame();
+
+        Assert.AreEqual(0, GameObject.FindObjectsOfType<TestItem>().Length);
     }
 }
--- Assets/Tests/TestContainer.cs
+++ Assets/Tests/TestContainer.cs
@@ -3,19 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using BasRoding.UnityContainer;
 
-public class TestContainer : PoolContainer<TestItem, Weapon> {
-
-    private void Start() {
-        List<Weapon> weapons = new List<Weapon>();
-        weapons.Add(new Weapon());
-        weapons.Add(new Weapon());
-        weapons.Add(new Weapon());
-        weapons.Add(new Weapon());
-        weapons.Add(new Weapon());
-
-        UpdateContainer(weapons);
-    }
-
+public class TestContainer : Container<TestItem, Weapon> {
 }
 
 public class Weapon {

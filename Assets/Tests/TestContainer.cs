83a6a1961fac3c096ac159e643484b8c9085692c
Bas Roding <bas.roding@gmail.com>
2018-10-11 11:30:29 +0200

    Added test framework

---
+++ Assets/Tests/TestContainer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BasRoding.UnityContainer;
+
+public class TestContainer : PoolContainer<TestItem, Weapon> {
+
+    private void Start() {
+        List<Weapon> weapons = new List<Weapon>();
+        weapons.Add(new Weapon());
+        weapons.Add(new Weapon());
+        weapons.Add(new Weapon());
+        weapons.Add(new Weapon());
+        weapons.Add(new Weapon());
+
+        UpdateContainer(weapons);
+    }
+
+}
+
+public class Weapon {
+
+}
---
+++ Assets/Tests/TestItem.cs
@@ -0,0 +1,7 @@
+﻿using BasRoding.UnityContainer;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestItem : ContainerItem<Weapon> {
+}
